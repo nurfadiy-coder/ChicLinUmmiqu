f512ed51e19906c2bad9c7c6b33209db5c663de1
nurfadiy-coder <nurfadiyah0707@gmail.com>
2026-05-09 18:22:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    publicDir: 'public', // Ini memastikan folder public ikut terbawa
+})
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
